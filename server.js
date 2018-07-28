772d2a1b01c21a8b15c2f833f6180a2411690eb1
Emily Morris <morris.emilyanne@gmail.com>
2018-07-27 21:45:59 -0500

    Added GET req for /podcasts endpoint

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 'use strict';
 
 const express = require('express');
+const morgan = require('morgan');
+const apiKey = 'FLPn3vd3iImshX0Wz9QLpJuAAYcop1Ml2jVjsnH8QFlK0EEpfK';
+const unirest = require('unirest');
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(morgan('combined'));
+
+function searchPodcasts(query, res) {
+	unirest.get("https://listennotes.p.mashape.com/api/v1/search?language=English&q="+ query +"&type=podcast")
+		.header("X-Mashape-Key", "FLPn3vd3iImshX0Wz9QLpJuAAYcop1Ml2jVjsnH8QFlK0EEpfK")
+		.header("Accept", "application/json")
+		.end(function (result) {
+		  res.send(result.body);
+	});
+}
+
+
+app.get('/podcasts', (req, res) => {
+	console.log(req.query);
+	searchPodcasts(req.query.q, res);
+});
+
 
 if (require.main === module) {
   app.listen(process.env.PORT || 8080, function() {
