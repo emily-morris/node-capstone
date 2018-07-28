aa769dac528b6b2f160b32b7fc277dca349e4203
Emily Morris <morris.emilyanne@gmail.com>
2018-07-28 13:57:43 -0500

    Set up submit form to get data from API

--- public/app.js
+++ public/app.js
@@ -1,8 +1,20 @@
-// user submits search form
-$('.search-form').submit(event => {
-	console.log($('.query').val());
-	event.preventDefault();
-	$.get('/podcasts?q=' + $('.query').val(), function(data) {
-		console.log('works');
+'use strict';
+
+$(document).ready(function () {
+	$('section').hide();
+	// user submits search form
+	$('.search-form').submit(event => {
+		event.preventDefault();
+		$('main').hide();
+		$('section').show();
+		$.get('/podcasts?q=' + $('.query').val(), function(data) {
+			// search results populate
+			data.results.forEach(function(item) {
+				$('section').append('<p>' + item.title_original + '<br>' + 'by ' + item.publisher_original + '<br>' + item.description_original + '</p>');
+			});
+		});
+	});
+	$('.new-search').click(event => {
+		location.reload();
 	});
 });
