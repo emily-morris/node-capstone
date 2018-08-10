0f294f9cb70b330ad249970642633a36cd3b10a9
Emily Morris <morris.emilyanne@gmail.com>
2018-08-10 11:44:31 -0500

    updated test

--- test/test-server.js
+++ test/test-server.js
@@ -2,30 +2,43 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../server.js');
+const mongoose = require('mongoose');
 
+// make expect syntax available
 const expect = chai.expect;
+
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
-describe('index', function() {
-	it('should return 200', function() {
-		return chai
-			.request(app)
+describe('index page', function() {
+	before(function() {
+		return runServer(TEST_DATABASE_URL);
+	});
+
+	after(function() {
+		return closeServer();
+	});
+
+	it('should exist', function() {
+		let res;
+		return chai.request(app)
 			.get('/')
-			.then(function(res) {
+			.then(function(_res) {
+				res = _res;
 				expect(res).to.have.status(200);
 			});
 	});
 });
 
-describe('app', function() {
-	it('should return 200', function() {
-		return chai
-			.request(app)
-			.get('/')
-			.then(function(res) {
-				expect(res).to.have.status(200);
-			});
-	});
-});
+// describe('app', function() {
+// 	it('should return 200', function() {
+// 		return chai
+// 			.request(app)
+// 			.get('/')
+// 			.then(function(res) {
+// 				expect(res).to.have.status(200);
+// 			});
+// 	});
+// });
