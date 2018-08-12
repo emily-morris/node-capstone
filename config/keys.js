9a595e1ae82ee8534e2963cffd33848d72721097
Emily Morris <morris.emilyanne@gmail.com>
2018-08-12 13:51:08 -0500

    tweak css

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+// add this file to .gitignore
+
+module.exports = {
+	google: {
+		clientID: '206810354724-hu2i241p0a4hudaeabqa25ksmnejge1o.apps.googleusercontent.com',
+		clientSecret: 'ZZy2KmXXV4MWEmGSmfsvHxcw'
+	},
+	mongodb: {
+		dbURI:'mongodb://demo-user:password1@ds133816.mlab.com:33816/podcasts-app'
+	}
+};
